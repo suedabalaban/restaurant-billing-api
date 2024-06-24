25949c66d656fd1d0d9cca61ee29d6793e89fe1d
[suedabalaban] <suedabalaban@gmail.com>
2024-06-24 17:23:47 +0300

    Add project files

---
+++ src/test/java/com/restaurant/billingsystem/BillingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.billingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
