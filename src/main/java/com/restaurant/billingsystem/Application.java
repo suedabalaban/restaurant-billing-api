3ad55905a3a5de3941f072d095db788494755e89
[suedabalaban] <suedabalaban@gmail.com>
2024-06-26 14:20:03 +0300

    Add application.java

--- src/main/java/com/restaurant/billingsystem/Application.java
+++ src/main/java/com/restaurant/billingsystem/Application.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BillingSystemApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BillingSystemApplication.class, args);
-	}
-
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
