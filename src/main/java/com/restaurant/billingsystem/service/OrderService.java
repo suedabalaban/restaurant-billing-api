9eb93f23be428da5faea23fca5b5fa80676233ec
[suedabalaban] <suedabalaban@gmail.com>
2024-06-29 20:32:32 +0300

    Add MenuService to order with an existing menuItem

--- src/main/java/com/restaurant/billingsystem/service/OrderService.java
+++ src/main/java/com/restaurant/billingsystem/service/OrderService.java
@@ -1,6 +1,8 @@
 package com.restaurant.billingsystem.service;
 
+import com.restaurant.billingsystem.model.MenuItem;
 import com.restaurant.billingsystem.model.Order;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -11,51 +13,78 @@ public class OrderService {
     private Map<Integer, Order> orders;
     private int nextOrderId;
 
-    public OrderService(){
+    @Autowired
+    private MenuService menuService;
+
+    public OrderService() {
         orders = new HashMap<>();
         nextOrderId = 1;
     }
 
     //Add order
-    public Order addOrder(Order order){
+    public Order addOrder(Order order) {
         int orderId = nextOrderId++;
         order.setOrderId(orderId);
         orders.put(orderId, order);
         return order;
     }
+
     //Delete order
-    public void deleteOrder(int orderId){
-        if(orders.containsKey(orderId)){
+    public void deleteOrder(int orderId) {
+        if (orders.containsKey(orderId)) {
             orders.remove(orderId);
-        }else{
+        } else {
             System.out.println("Order ID not found.");
         }
     }
+
     //Update order
-    public Order updateOrder(int orderId, Order updatedOrder){
-        if(orders.containsKey(orderId)){
+    public Order updateOrder(int orderId, Order updatedOrder) {
+        if (orders.containsKey(orderId)) {
             orders.put(orderId, updatedOrder);
-        }else{
-            System.out.println("Order ID not found.");   
+            return updatedOrder;
+        } else {
+            System.out.println("Order ID not found.");
+            return null;
         }
-        return null;
     }
+
     //Find order by their id
-    public Order getOrderById(int orderId){
+    public Order getOrderById(int orderId) {
         return orders.get(orderId);
     }
+
     //List all orders
-    public Map<Integer, Order> getAllOrders(){
+    public Map<Integer, Order> getAllOrders() {
         return orders;
     }
+
     //Calculate total price of the order by orderId
-    public Double calculateTotalPrice(int orderId){
+    public Double calculateTotalPrice(int orderId) {
         Order order = orders.get(orderId);
-        if(order != null){
+        if (order != null) {
             return order.getTotalAmount();
-        }else{
-            System.out.println("Order ID not found."); 
-        } 
-        return null;
+        } else {
+            System.out.println("Order ID not found.");
+            return null;
+        }
+    }
+
+    //Add item to order by menuItem id
+    public Order addItemToOrder(int orderId, int itemId) {
+        Order order = orders.get(orderId);
+        if (order != null) {
+            MenuItem menuItem = menuService.getMenuItemById(itemId);
+            if (menuItem != null) {
+                order.addItem(menuItem);
+                return order;
+            } else {
+                System.out.println("MenuItem with id " + itemId + " not found.");
+                return null;
+            }
+        } else {
+            System.out.println("Order ID not found.");
+            return null;
+        }
     }
 }
