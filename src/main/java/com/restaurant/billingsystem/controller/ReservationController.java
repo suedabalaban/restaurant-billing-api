60cf974c4883bb1208696bc94a7e6af4dafc277d
[suedabalaban] <suedabalaban@gmail.com>
2024-07-02 17:47:46 +0300

    Delete assign reservation to table function

--- src/main/java/com/restaurant/billingsystem/controller/ReservationController.java
+++ src/main/java/com/restaurant/billingsystem/controller/ReservationController.java
@@ -1,8 +1,6 @@
 package com.restaurant.billingsystem.controller;
 
 import com.restaurant.billingsystem.model.Reservation;
-import com.restaurant.billingsystem.model.Table;
-import com.restaurant.billingsystem.service.TableService;
 import com.restaurant.billingsystem.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +13,6 @@ import java.util.List;
 public class ReservationController{
     @Autowired
     private ReservationService reservationService;
-
-    @Autowired 
-    private TableService tableService;
 
     @GetMapping
     public List<Reservation> getAllReservations() {
@@ -43,13 +38,4 @@ public class ReservationController{
     public void deleteReservation(@PathVariable int id) {
         reservationService.deleteReservation(id);
     }
-
-    @PostMapping("/{reservationId}/assignTable/{tableId}")
-    public void assignReservationToTable(@PathVariable int reservationId, @PathVariable int tableId) {
-        Table table = tableService.getTableById(tableId);
-        if (table != null) {
-            reservationService.assignReservationToTable(reservationId, table);
-        }
-    }
-    
 }
